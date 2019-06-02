0bc2565a055ebee6dd6ad0f1e59bf609355ace6f
wz5mz <wz5mz@virginia.edu>
2019-06-02 00:06:54 -0700

    Update track_spend.py

--- track_spend.py
+++ track_spend.py
@@ -21,10 +21,10 @@ card = card_dict.get(re.search("\d\d\d\d", card).group(0))
 def track_spend(date, amount, card, tag, memo):
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-#    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-#        '/data/data/com.termux/files/home/storage/downloads/Track Spend-200ce7a40f14.json', scope)
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'c:/users/wade/downloads/Track Spend-908a12e5ca6e.json', scope)
+        '/data/data/com.termux/files/home/storage/downloads/Track Spend-908a12e5ca6e.json', scope)
+#    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#        'c:/users/wade/downloads/Track Spend-908a12e5ca6e.json', scope)
     gc = gspread.authorize(credentials)
     worksheet = gc.open('Wade Zhang\'s Spending from 2016-06-20.xlsx').worksheet('2016-11-17 to')
     last_row = len(worksheet.col_values(2)) + 1
