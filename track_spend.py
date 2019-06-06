b6ba1584628a7b05380b2c1bcffa4e9e0ca99373
wz5mz <wz5mz@virginia.edu>
2019-06-05 23:39:25 -0700

    Update track_spend.py

--- track_spend.py
+++ track_spend.py
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/env python3
+#! /data/data/com.termux/files/usr/bin/env python3
 
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
@@ -11,13 +11,13 @@ tag = sys.argv[4]
 card = sys.argv[3]
 memo = sys.argv[5]
 
-amount = float(re.search('.+\.\d\d', amount).group(0)[1:])
+amount = float(re.search('\$.+\.\d\d', amount).group(0)[1:])
 
 card_dict = {"2006": "AmEx Gold Card",
              "1003": "AmEx Gold Card",
              "1004": "AmEx Gold Card",
              "0000": "Citi Double Cash credit",
-             "": "AmegyBank Amazing credit"}
+             "2169": "AmegyBank Amazing credit"}
 card = card_dict.get(re.search('\d\d\d\d', card).group(0))
 
 
@@ -26,8 +26,8 @@ def track_spend(date, amount, card, tag, memo):
              'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         '/data/data/com.termux/files/home/storage/downloads/Track Spend-908a12e5ca6e.json', scope)
-#    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-#        'c:/users/wade/downloads/Track Spend-908a12e5ca6e.json', scope)
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    #     'c:/users/wade/downloads/Track Spend-908a12e5ca6e.json', scope)
     gc = gspread.authorize(credentials)
     worksheet = gc.open('Wade Zhang\'s Spending from 2016-06-20.xlsx').worksheet('2016-11-17 to')
     last_row = len(worksheet.col_values(2)) + 1
