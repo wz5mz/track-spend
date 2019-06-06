05072d462250c946e1a6c6751aa7a4c7d94a51b1
wz5mz <wz5mz@virginia.edu>
2019-06-06 11:16:06 -0700

    Update track_spend.py

--- track_spend.py
+++ track_spend.py
@@ -7,20 +7,17 @@ import re
 
 date = sys.argv[1]
 amount = sys.argv[2]
-tag = sys.argv[4]
 card = sys.argv[3]
+tag = sys.argv[4]
 memo = sys.argv[5]
 
 amount = float(re.search('\$.+\.\d\d', amount).group(0)[1:])
-print(amount)
 
 card_dict = {"2006": "AmEx Gold Card",
              "1003": "AmEx Gold Card",
              "1004": "AmEx Gold Card",
              "0000": "Citi Double Cash credit",
              "2169": "AmegyBank Amazing credit"}
-print(card)
-print(re.search('\d\d\d\d', card).group(0))
 card = card_dict.get(re.search('\d\d\d\d', card).group(0))
 
 
@@ -34,7 +31,7 @@ def track_spend(date, amount, card, tag, memo):
     gc = gspread.authorize(credentials)
     worksheet = gc.open('Wade Zhang\'s Spending from 2016-06-20.xlsx').worksheet('2016-11-17 to')
     last_row = len(worksheet.col_values(2)) + 1
-    cell_list = [gspread.models.Cell(last_row, 1, date),
+    cell_list = [gspread.models.Cell(last_row, 1, str(date)),
                  gspread.models.Cell(last_row, 2, amount),
                  gspread.models.Cell(last_row, 3, card),
                  gspread.models.Cell(last_row, 4, tag),
