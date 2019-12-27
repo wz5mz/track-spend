81b71249158fad21535587078bfe5f89ab49bda4
wz5mz <wz5mz@virginia.edu>
2019-12-26 19:27:55 -0800

    Update track_spend.py

--- track_spend.py
+++ track_spend.py
@@ -10,14 +10,13 @@ amount = sys.argv[2]
 card = sys.argv[3]
 tag = sys.argv[4]
 memo = sys.argv[5]
+tip = sys.argv[6]
 
-amount = float(re.search('\$.+\.\d\d', amount).group(0)[1:])
+amount = float(re.search('\$.+\.\d\d', amount).group(0)[1:]) + float(tip)
 
-card_dict = {"2006": "AmEx Gold Card",
-             "1003": "AmEx Gold Card",
-             "1004": "AmEx Gold Card",
-             "0000": "Citi Double Cash credit",
-             "2169": "AmegyBank Amazing credit"}
+card_dict = {"8420": "Capital One Quicksilver",
+             "1311": "Capital One Savor credit",
+             "9057": "Charles Schwab personal checking"}
 card = card_dict.get(re.search('\d\d\d\d', card).group(0))
 
 
@@ -31,11 +30,11 @@ def track_spend(date, amount, card, tag, memo):
     gc = gspread.authorize(credentials)
     worksheet = gc.open('Wade Zhang\'s Spending from 2016-06-20.xlsx').worksheet('2016-11-17 to')
     last_row = len(worksheet.col_values(2)) + 1
-    cell_list = [gspread.models.Cell(last_row, 1, date.strip()),
+    cell_list = [gspread.models.Cell(last_row, 1, date),
                  gspread.models.Cell(last_row, 2, amount),
                  gspread.models.Cell(last_row, 3, card),
-                 gspread.models.Cell(last_row, 4, tag.strip()),
-                 gspread.models.Cell(last_row, 5, memo.strip())]
+                 gspread.models.Cell(last_row, 4, tag),
+                 gspread.models.Cell(last_row, 5, memo)]
     worksheet.update_cells(cell_list)
 
 
