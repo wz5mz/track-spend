1913ca194baff8916d7aa2b6184bca428245f4c7
wz5mz <wz5mz@virginia.edu>
2019-12-31 11:05:58 -0800

    Create schwab_report.py

---
+++ schwab_report.py
@@ -0,0 +1,53 @@
+import datetime
+import ofxtools
+import re
+import os
+from lxml import etree
+import keyring
+
+
+# Include error handling for when signon fails - MAYBE RETURN N/A?
+
+# def schwab_report(dtstart, dtend):
+
+
+dtstart = datetime.datetime(2019, 1, 1, tzinfo=ofxtools.utils.UTC)
+dtend = datetime.datetime(2019, 12, 25, tzinfo=ofxtools.utils.UTC)
+
+
+username = keyring.get_password('schwab', 'username')
+password = keyring.get_password('schwab', 'password')
+rothacctno = keyring.get_password('schwab', 'rothacctno')
+brokerageacctno = keyring.get_password('schwab', 'brokerageacctno')
+
+client = ofxtools.OFXClient('https://ofx.schwab.com/cgi_dev/ofx_server',
+                            userid=username,
+                            brokerid='SCHWAB.COM')
+
+def getnetworth():
+    networth = 0
+    cash = 0
+    acctid = [brokerageacctno, rothacctno]
+    for acct in acctid:
+        statement = ofxtools.Client.InvStmtRq(acctid=acct, dtstart=dtstart, dtend=dtend)
+        with client.request_statements(password, statement) as f:
+            message = f.read()
+
+        file = open('schwab_statement.xml', 'w')
+        file.write(message.decode())
+        file.close()
+
+        tree = etree.parse('schwab_statement.xml')
+        root = tree.getroot()
+
+        for element in root.iter():
+            if element.tag=='NAME' and element.text=='Total Account Value':
+                for sibling in element.getparent():
+                    if sibling.tag=='VALUE':
+                        networth += float(sibling.text)
+            if element.tag=='AVAILCASH':
+                cash += float(element.text)
+    return networth, cash
+
+os.remove('schwab_statement.xml')
+print(getnetworth())
